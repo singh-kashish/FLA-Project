6d9c158cbbd125eef1e854421ca62e8b01849b77
Kashish Singh <ks1147@srmist.edu.in>
2020-10-03 19:23:55 +0000

    working rooutes

---
+++ app.js
@@ -0,0 +1,37 @@
+var express 		= 		require("express"),
+	bodyParser		=		require("body-parser");
+var app				=		express();
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/",function(req,res){
+	res.render("landing");
+});
+app.get("/dfa",function(req,res){
+	res.render("dfa");
+})
+app.get("/nfa",function(req,res){
+	res.render("nfa");
+})
+app.get("/algorithm",function(req,res){
+	res.render("algorithm");
+})
+app.get("/convertor",function(req,res){
+	res.render("../views/convertor/index");
+})
+
+
+
+
+
+
+
+
+
+
+var port=process.env.PORT || 3000;
+app.listen(port,process.env.IP,function(){
+	console.log("Fla server running");
+})
